fcb0e67f6c257d5f19788eaf1eb69b842f62e7f8
mtnjoroge <mtnjoroge@gmail.com>
2015-07-26 17:16:16 +0300

    columns set to pop up at random height

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -5,6 +5,8 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
@@ -14,11 +16,15 @@ public class FlappyBird implements ActionListener
  
 public static FlappyBird flappyBird;
 
-public final int WIDTH = 600, HEIGHT = 600;
+public final int WIDTH = 700, HEIGHT = 700;
 
 public Render render;
 
 public Rectangle bird;
+
+public ArrayList<Rectangle> columns;
+
+public Random rand;
 
 // class constructor
   public FlappyBird()
@@ -27,34 +33,58 @@ public Rectangle bird;
 	Timer timer = new Timer(20, this);
 	
 	render = new Render();
+	rand = new Random();
 	
 	jframe.add(render);
 	jframe.setTitle("FlappyBird");
 	jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	jframe.setSize(WIDTH, HEIGHT);
 	jframe.setResizable(false);
-	jframe.setVisible(true);
+	jframe.setVisible(true); 
 	
 	bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
+	columns = new ArrayList<Rectangle>();
 	
 	timer.start();
    } 
 	
-    
+  //adding the columns
+public void addColumn(boolean start)
+{
+ int space = 300;
+ int width = 100;
+ int height = 50 + rand.nextInt(300);
+ 
+  if (start)
+  {  
+ columns.add(new Rectangle(WIDTH  + width + columns.size() * 300, HEIGHT - height - 120, width, height));
+ columns.add(new Rectangle(WIDTH + width + (columns.size() + 1) * 300, 0, width, HEIGHT - height - space ));
+  }
+  else
+  {
+	  columns.add(new Rectangle(columns.get(columns.size() + 1).x + 600, HEIGHT - height - 120, width, height));
+	  columns.add(new Rectangle(WIDTH + width + (columns.size() + 1).x, 0, width, HEIGHT - height - space ));  
+  }
+  
+  
+ 
+}
+
+
+//creating the columns
+public void paintColumn(Graphics g, Rectangle column)
+{
+	g.setColor(Color.green.darker());
+	g.fillRect(column.x, column.y, column.width, column.height);
+}  
+  
 @Override
 public void actionPerformed(ActionEvent e) 
  {
   render.repaint(); 	
   }
 
-  //creating the columns
-public void paintColumn(Graphics g, Rectangle column)
-{
-	g.setColor(Color.green.darker());
-	g.fillRect(column.x, column.y, column.width, column.height);
-}
-
-  public void repaint(Graphics g)
+ public void repaint(Graphics g)
   {
 	  //background paint components
 	g.setColor(Color.cyan);
