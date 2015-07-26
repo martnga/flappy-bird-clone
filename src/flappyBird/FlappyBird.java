d319bc25e654915dfb0cd97d5bebe26e5415cf51
mtnjoroge <mtnjoroge@gmail.com>
2015-07-26 15:51:48 +0300

    added render for the jframe

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -7,13 +7,17 @@ public class FlappyBird {
  
 public static FlappyBird flappyBird;
 
-public final int WIDTH = 800, HEIGHT = 800;
+public final int WIDTH = 600, HEIGHT = 600;
+public Render render;
 
 // class constructor
   public FlappyBird()
   {
 	JFrame jframe = new JFrame();
-		
+	
+	render = new Render();
+	
+	jframe.add(render);
 	jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	jframe.setSize(WIDTH, HEIGHT);
 	jframe.setResizable(false);
--- src/flappyBird/Render.java
+++ src/flappyBird/Render.java
@@ -1,10 +1,23 @@
 package flappyBird;
 
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
-public class Render extends JPanel {
+public class Render extends JPanel 
+{
 
 	
-	private static final long serialVersionUID = -8171341062077512766L;
+  private static final long serialVersionUID = -8171341062077512766L;
+	
+  @Override
+  protected void paintComponent(Graphics g) 
+  {
+		
+  super.paintComponent(g);
+		
+  FlappyBird.flappyBird.repaint(g);
+  
+  }
 
 }
