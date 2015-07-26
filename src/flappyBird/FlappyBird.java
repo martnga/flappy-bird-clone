3f758f80f632e3f8ad5f49537a2f5d576cfbfd9d
mtnjoroge <mtnjoroge@gmail.com>
2015-07-26 15:45:52 +0300

    refractored the code/curlyBraces positioning

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -1,32 +1,36 @@
 package flappyBird;
 
 import java.awt.Graphics;
-
 import javax.swing.JFrame;
 
 public class FlappyBird {
-     
-	public static FlappyBird flappyBird;
-	
-	public final int WIDTH = 800, HEIGHT = 800;
-	
-	// class constructor
-	public FlappyBird(){
-		JFrame jframe = new JFrame();
-		
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jframe.setSize(WIDTH, HEIGHT);
-		jframe.setResizable(false);
-		jframe.setVisible(true);
-	}
-		
-	public static void main(String[] args){
-		flappyBird = new FlappyBird();
-	}
+ 
+public static FlappyBird flappyBird;
 
-	public void repaint(Graphics g) {
-		// TODO Auto-generated method stub
+public final int WIDTH = 800, HEIGHT = 800;
+
+// class constructor
+  public FlappyBird()
+  {
+	JFrame jframe = new JFrame();
 		
-	}
+	jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	jframe.setSize(WIDTH, HEIGHT);
+	jframe.setResizable(false);
+	jframe.setVisible(true);
+   }
+		
+  public void repaint(Graphics g)
+  {
+		
+		
+  }
+  
+  public static void main(String[] args)
+  {
+	flappyBird = new FlappyBird();
+  }
+
+	
 	
 }
