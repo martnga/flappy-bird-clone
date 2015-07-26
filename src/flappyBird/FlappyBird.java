af6d1cebd1b14feb46d183dc21acd3f8eda67c6b
mtnjoroge <mtnjoroge@gmail.com>
2015-07-26 15:20:11 +0300

    added a JFrame in FB constructor class

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -1,14 +1,22 @@
 package flappyBird;
 
-public class flappyBird {
+import javax.swing.JFrame;
 
-	public static flappy
+public class FlappyBird {
+     
+	public static FlappyBird flappyBird;
+	
+	public final int WIDTH = 800, HEIGHT = 800;
+	
+	// class constructor
 	public FlappyBird(){
+		JFrame jframe = new JFrame();
 		
+		jframe.setSize(WIDTH, HEIGHT);
 	}
 		
 	public static void main(String[] args){
-		
+		flappyBird = new FlappyBird();
 	}
 	
 }
